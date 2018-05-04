613bd8dbe717c06e4dd2cfff3fbfda1580150f18
Carey Johnson <johnson.careyj@gmail.com>
2018-05-04 16:58:54 -0700

    Finish Phase If with Lite

--- Active_Record_Lite/lib/01_sql_object.rb
+++ Active_Record_Lite/lib/01_sql_object.rb
@@ -5,15 +5,43 @@ require 'active_support/inflector'
 
 class SQLObject
 
-  def initialize
-    @table_name
-  end
+
 
   def self.columns
+    return @cols if @cols
+    var = DBConnection.execute2(<<-SQL).first
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      LIMIT
+      0
+    SQL
+
+    cols = var.map do |el|
+      el.to_sym
+    end
+    @cols = cols
 
   end
 
   def self.finalize!
+
+    cols = self.columns
+    cols.each do |col|
+
+    define_method(col) do
+      attributes[col]
+      # instance_variable_get("@#{col}")
+    end
+
+    define_method("#{col}=") do |value|
+      attributes[col]=value
+      # instance_variable_set("@#{name}", value)
+    end
+  end
+
+
   end
 
   def self.table_name=(table_name)
@@ -21,31 +49,51 @@ class SQLObject
   end
 
   def self.table_name
-    self.to_s.underscore + "s"
+    @table_name = self.to_s.underscore + "s"
   end
 
   def self.all
-    # ...
+    var = DBConnection.execute(<<-SQL)
+      SELECT #{self.table_name}.*
+      FROM
+        #{self.table_name}
+    SQL
+
+    self.parse_all(var)
+
   end
 
   def self.parse_all(results)
-    # ...
+    results.map do |result|
+      self.new(result)
+    end
   end
 
   def self.find(id)
-    # ...
+    possible = self.all
+    possible.each do |pos|
+      return pos if pos.id == id
+    end
+    nil
   end
 
   def initialize(params = {})
-    # ...
+    params.each do |k, v|
+      if self.class.columns.include?(k.to_sym)
+        self.send("#{k.to_sym}=", v)
+      else
+        raise "unknown attribute '#{k.to_sym}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    return @attributes if @attributes
+    @attributes = {}
   end
 
   def attribute_values
-    # ...
+    
   end
 
   def insert
